955210a59736cee6955a3a69b70fc938ceeb2640
vfacl9w <126799061+vfacl9w@users.noreply.github.com>
2023-03-02 19:55:43 +0300

    2 files modified

--- Vehicle.ts
+++ Vehicle.ts
@@ -1,9 +1,11 @@
-export class  Vehicle {
+export class Vehicle {
  private id:number;
  constructor(id:number)
  {
    this.id =id;
 
  }
- 
+ public getId():number{
+   return this.id;
+ }
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-import { Vehicle } from "./Vehicle";
-
-const app = document.getElementById("app");
+import { Vehicle } from './Vehicle';
+const app = document.getElementById('app');
 // 2. Create a new <p></p> element programmatically
-const p = document.createElement("p");
+const p = document.createElement('p');
 // 3. Add the text content
-p.textContent = "Hello, World!";
+p.textContent = 'Hello, World!';
 // 4. Append the p element to the div element
 app?.appendChild(p);
 const demoVehicle = new Vehicle(5);
